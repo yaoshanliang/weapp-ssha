2b9f2b4d5157e5783be4b5fa130e72437b5f9a19
iat <i@iat.net.cn>
2017-01-10 16:19:21 +0800

    add video

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,23 +5,11 @@ Page({
       {
         id: 'introduction',
         name: '公司介绍',
-        open: false,
-        // pages: ['view', 'scroll-view', 'swiper']
+      },
+      {
+        id: 'video',
+        name: '宣传片',
       }
     ]
-  },
-  widgetsToggle: function (e) {
-    var id = e.currentTarget.id, list = this.data.list;
-    console.log(id);
-    for (var i = 0, len = list.length; i < len; ++i) {
-      if (list[i].id == id) {
-        list[i].open = !list[i].open;
-      } else {
-        list[i].open = false;
-      }
-    }
-    this.setData({
-      list: list
-    });
   }
 });
---
+++ pages/video/video.js
@@ -0,0 +1,62 @@
+// pages/video/video.js
+function getRandomColor () {
+  let rgb = []
+  for (let i = 0 ; i < 3; ++i){
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length == 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
+
+Page({
+    data:{
+    src: "http://www.jsssha.com/wp-content/uploads/2016/11/%E6%8C%82%E7%89%8C.mp4"  
+  },
+  onReady: function (res) {
+    this.videoContext = wx.createVideoContext('myVideo')
+  },
+  inputValue: '',
+    data: {
+        src: '',
+    danmuList: [
+      {
+        text: '盛世华安好棒',
+        color: '#ff0000',
+        time: 1
+      },
+      {
+        text: '可以发弹幕居然',
+        color: '#ff00ff',
+        time: 2
+      },
+      {
+        text: '精彩的在后面',
+        color: '#ff0000',
+        time: 3
+      }
+    ]
+    },
+  bindInputBlur: function(e) {
+    this.inputValue = e.detail.value
+  },
+  bindButtonTap: function() {
+    var that = this
+    wx.chooseVideo({
+      sourceType: ['album', 'camera'],
+      maxDuration: 60,
+      camera: ['front','back'],
+      success: function(res) {
+        that.setData({
+          src: res.tempFilePath
+        })
+      }
+    })
+  },
+  bindSendDanmu: function () {
+    this.videoContext.sendDanmu({
+      text: this.inputValue,
+      color: getRandomColor()
+    })
+  }
+})
