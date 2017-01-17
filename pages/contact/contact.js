31d0e46196830d9eef9600d84d63f4d6ee148706
iat <i@iat.net.cn>
2017-01-17 13:38:14 +0800

    update to v2 using api

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -1,13 +1,47 @@
+var CONFIG = require('../../utils/config.js')
+var WxParse = require('../../utils/wxParse/wxParse.js');
+
 Page({
   data: {
-    title: '联系我们',
-    latitude: 31.838403,
-    longitude: 120.528958,
-    scale: 16,
-    markers: [{
-      latitude: 31.838403,
-      longitude: 120.528958,
-      name: '华东国际大厦5F'
-    }]
+  },
+  onLoad: function () {
+    var that = this;
+    wx.request({
+      url: CONFIG.API_URL.GET_PAGE + '36',
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function(res) {
+        console.log(res);
+
+        if (res.statusCode == 200 && res.data.status == 'ok') {
+          var data = res.data;
+          
+          that.setData({page: data.page});
+          WxParse.wxParse('content', 'html', data.page.content, that, 25)
+        } else {
+          
+        }
+      }
+    })
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  },
+  go: function(event) {
+    wx.navigateTo({
+      url: '/pages/news/news-details?id=' + event.currentTarget.dataset.type
+    })
   }
 })
